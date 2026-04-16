a6c7ef67bf405169898b58b7048cb3e6ce9f9d52
JABA <jabahsigma@gmail.com>
2026-04-16 20:40:41 +0400

    Include script for Murder vs Sheriff
    Added support for Murder vs Sheriff game.

--- JABAHUB.lua
+++ JABAHUB.lua
@@ -9,4 +9,7 @@ elseif placeId == 9872472334 then
 elseif placeId == 130645793692787 or placeId == 142823291 then
     -- MM2 and MMV games
     loadstring(game:HttpGet('https://raw.githubusercontent.com/jabahsigma-collab/Build-A-Boat-Script/refs/heads/main/Mm2%20And%20Mmv'))()
+elseif placeid == 12355337193 then
+    --Murder vs Sheriff
+    loadstring(game:HttpGet("https://pastebin.com/raw/tnJVf9mD"))()
 end
